293de57c727def93a4d26d003def044d40100d01
Blake <blameswalker@gmail.com>
2023-09-14 20:51:39 -0700

    Remove newlines to improve readability

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,6 +1,38 @@
 def merge_sort(array)
-  # split array
-  # sort left half
-  # sort right half
-  # merge halves
+  n = array.size
+
+  if n < 2
+    array
+  else
+    left_half = merge_sort(array[0...n/2])
+    right_half = merge_sort(array[n/2..-1])
+    
+    merge(left_half, right_half)
+  end
 end
+
+# example array: [1, 2, 3, 4, 5, 6, 7]
+
+def merge(left_half, right_half, sorted_array = [])
+  if left_half.empty?
+    sorted_array << right_half
+  elsif right_half.empty?
+    sorted_array << left_half
+  else
+    if left_half.first >= right_half.first
+      sorted_array << right_half.shift
+    else
+      sorted_array << left_half.shift
+    end
+    
+    merge(left_half, right_half, sorted_array)
+  end
+
+  sorted_array.flatten
+end
+
+array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19].shuffle
+
+print array
+puts "\n\n"
+print merge_sort(array)
